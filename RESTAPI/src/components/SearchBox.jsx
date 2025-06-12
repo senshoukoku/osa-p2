a9c248a91a445682e459ae3a31f5254407f8ec5c
senshoukoku <kurtset.22@gmail.com>
2025-06-12 11:14:16 +0800

    Delete RESTAPI/src directory

--- RESTAPI/src/components/SearchBox.jsx
+++
@@ -1,9 +0,0 @@
-function SearchBox ({value, onChange}) {
-    return(
-        <div>
-            <label>Search for a Country (full country name) </label> 
-            <input value={value} onChange={(e) => onChange(e.target.value)}/>
-        </div>
-    )
-}
-export default SearchBox
